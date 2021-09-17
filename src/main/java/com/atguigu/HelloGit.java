6aac963e89c2c74ae425f7848907735724182ffe
Jianlin <hssflkjashlkdfjs@qq.com>
2021-09-17 11:18:50 +0800

    1.1

---
+++ src/main/java/com/atguigu/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author Jianlin
+ * @create 2021-09-17 11:16
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!  Hello Git!!!");
+    }
+}
